7140b41df4d49164521829dddf48c8e4e11307ad
jnkmmdndmd-arch <jnkmmdndmd@gmail.com>
2026-09-22 14:44:39 +0300

    feat: add Minecraft 1.17.1 crafting recipes

--- scripts/data/recipe_registry.gd
+++ scripts/data/recipe_registry.gd
@@ -11,7 +11,31 @@ var recipes: Array[Dictionary] = [
     {"id":"iron_sword","inputs":{"8":2,"12":3},"output":{"132":1}},
     {"id":"camp_food","inputs":{"3":2,"50":1},"output":{"200":2}},
     {"id":"chest","inputs":{"8":8},"output":{"20":1}},
-    {"id":"lantern","inputs":{"22":2,"13":1},"output":{"144":2}}
+    {"id":"lantern","inputs":{"22":2,"13":1},"output":{"144":2}},
+
+    {"id":"minecraft_sticks","inputs":{"8":2},"output":{"183":4}},
+    {"id":"minecraft_wooden_pickaxe","inputs":{"8":3,"183":2},"output":{"100":1}},
+    {"id":"minecraft_stone_pickaxe","inputs":{"2":3,"183":2},"output":{"101":1}},
+    {"id":"minecraft_iron_pickaxe","inputs":{"176":3,"183":2},"output":{"102":1}},
+    {"id":"minecraft_wooden_axe","inputs":{"8":3,"183":2},"output":{"110":1}},
+    {"id":"minecraft_stone_axe","inputs":{"2":3,"183":2},"output":{"111":1}},
+    {"id":"minecraft_iron_axe","inputs":{"176":3,"183":2},"output":{"112":1}},
+    {"id":"minecraft_wooden_shovel","inputs":{"8":1,"183":2},"output":{"120":1}},
+    {"id":"minecraft_stone_shovel","inputs":{"2":1,"183":2},"output":{"121":1}},
+    {"id":"minecraft_iron_shovel","inputs":{"176":1,"183":2},"output":{"122":1}},
+    {"id":"minecraft_wooden_sword","inputs":{"8":2,"183":1},"output":{"130":1}},
+    {"id":"minecraft_stone_sword","inputs":{"2":2,"183":1},"output":{"131":1}},
+    {"id":"minecraft_iron_sword","inputs":{"176":2,"183":1},"output":{"132":1}},
+    {"id":"minecraft_bow","inputs":{"183":3,"184":3},"output":{"140":1}},
+    {"id":"minecraft_arrow","inputs":{"187":1,"183":1,"185":1},"output":{"141":4}},
+    {"id":"minecraft_bucket","inputs":{"176":3},"output":{"143":1}},
+    {"id":"minecraft_shield","inputs":{"8":6,"176":1},"output":{"189":1}},
+    {"id":"minecraft_flint_and_steel","inputs":{"176":1,"187":1},"output":{"191":1}},
+    {"id":"minecraft_furnace","inputs":{"2":8},"output":{"19":1}},
+    {"id":"minecraft_chest","inputs":{"8":8},"output":{"20":1}},
+    {"id":"minecraft_crafting_table","inputs":{"8":4},"output":{"18":1}},
+    {"id":"minecraft_torch","inputs":{"174":1,"183":1},"output":{"22":4}},
+    {"id":"minecraft_coal_torch_from_charcoal","inputs":{"175":1,"183":1},"output":{"22":4}}
 ]
 func find_recipe(recipe_id:String)->Dictionary:
     for recipe in recipes:
